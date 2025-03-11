8e3386eca007a7ac3f4f3748ac1d95aef7ab39cb
KazukiMoriai <moriaikazuki@gmail.com>
2025-03-11 11:15:23 +0900

    set cd

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>表示できたよ！</h2>
+      <div>自動デプロイ完了</div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
